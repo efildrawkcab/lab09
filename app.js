cc202a9faf021ee20a820e5fd821e3c694f3b41f
Sean Ooi <69132873+seano188@users.noreply.github.com>
2021-02-24 15:00:15 +0800

    4th commit

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-
 const app = express();
+const userRouter = require("./routers/user.router");
 
 let PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello JWT");
